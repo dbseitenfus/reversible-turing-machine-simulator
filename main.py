433f286b20f8cfe0c79c041a1b53ef87b8885757
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-10-15 22:10:03 -0300

    feat: read states

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ number_of_states = first_line_data[0]
 number_of_symbols_input_alphabet = first_line_data[1]
 number_of_symbols_tape_alphabet = first_line_data[2]
 number_of_transitions = first_line_data[3]
+
+second_line = file.readline()
+states = second_line.replace('\n', '').split(' ')
