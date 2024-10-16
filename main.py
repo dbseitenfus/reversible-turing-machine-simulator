06bf454959777bd84612df0f74ed60a0563db99b
gsquevedo <gsquevedo@inf.ufsm.br>
2024-10-15 22:39:24 -0300

    estrutura de quintuplas

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+class Quintuple: 
+    def __init__(self, current_state, read_symbol, next_state, write_symbol, move_direction):
+        self.current_state = current_state
+        self.read_symbol = read_symbol
+        self.next_state = next_state
+        self.write_symbol = write_symbol
+        self.move_direction = move_direction
+
 file = open('entrada-quintupla.txt', 'r')
 
 first_line = file.readline()
@@ -10,8 +18,32 @@ first_line_data = first_line.split(' ')
 number_of_states = first_line_data[0]
 number_of_symbols_input_alphabet = first_line_data[1]
 number_of_symbols_tape_alphabet = first_line_data[2]
-number_of_transitions = first_line_data[3]
+number_of_transitions = int(first_line_data[3])
 
 states = second_line.replace('\n', '').split(' ')
 input_alphabet = third_line.replace('\n', '').split(' ')
 tape_alphabet = fourth_line.replace('\n', '').split(' ')
+
+quintuples = []
+
+for _ in range(number_of_transitions):
+    line = file.readline().strip()
+
+    parts = line.replace('(','').replace(')','').split('=')
+    left_part = parts[0].split(',')
+    right_part = parts[1].split(',')
+
+    quintuple = Quintuple(
+        current_state = int(left_part[0]),
+        read_symbol = left_part[1],
+        next_state = int(right_part[0]),
+        write_symbol = right_part[1],
+        move_direction = right_part[2]
+    )
+
+    quintuples.append(quintuple)
+
+file.close()
+
+for quintuple in quintuples:
+    print(f'({quintuple.current_state}, {quintuple.read_symbol}) = ({quintuple.next_state}, {quintuple.write_symbol}, {quintuple.move_direction})')
